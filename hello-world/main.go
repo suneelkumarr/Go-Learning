3ea29b04157d1d793cf8410ec95605e6282bbacd
suneelkumarr <sunilkumar05644@gmail.com>
2024-05-13 17:42:44 +0530

    add server and modified changes and add portnumber

--- hello-world/main.go
+++ hello-world/main.go
@@ -5,18 +5,39 @@ import (
 	"net/http"
 )
 
+var PortNumber = ":8080"
+
+func HomePage(w http.ResponseWriter, r *http.Request) {
+	// w.Write([]byte("Hello, World!"))
+
+	fmt.Fprintf(w, "This is home page")
+}
+
+func AboutPage(w http.ResponseWriter, r *http.Request) {
+	// w.Write([]byte("Hello, World!"))
+	sum := Addvalues(10, 20)
+	_, _ = fmt.Fprintf(w, fmt.Sprintf("Sum is : %d", sum))
+}
+
+func Addvalues(x, y int) int {
+	return x + y
+}
+
 func main() {
 	// fmt.Println("Hello, World!")
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		// w.Write([]byte("Hello, World!"))
-		n, err := fmt.Fprint(w, "Hello, World!")
+	// http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	// 	// w.Write([]byte("Hello, World!"))
+	// 	n, err := fmt.Fprint(w, "Hello, World!")
 
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		fmt.Println(fmt.Sprintf("Number of byte written : %d", n))
-	})
+	// 	if err != nil {
+	// 		fmt.Println(err)
+	// 		return
+	// 	}
+	// 	fmt.Println(fmt.Sprintf("Number of byte written : %d", n))
+	// })
 
-	_ = http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/", HomePage)
+	http.HandleFunc("/about", AboutPage)
+	fmt.Printf("Starting server on port %s", "http://localhost"+PortNumber)
+	_ = http.ListenAndServe(PortNumber, nil)
 }
