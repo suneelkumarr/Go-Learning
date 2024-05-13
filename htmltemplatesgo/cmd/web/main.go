00fb99a2f86d03193c287b3c6e1a84b2f50a2c51
suneelkumarr <sunilkumar05644@gmail.com>
2024-05-13 19:46:13 +0530

    added proper structure

--- htmltemplatesgo/cmd/web/main.go
+++ htmltemplatesgo/cmd/web/main.go
@@ -2,15 +2,18 @@ package main
 
 import (
 	"fmt"
+	"htmltemplatesgo/pkg/handlers"
 	"net/http"
 )
 
 var PortNumber = ":8080"
 
+//go run cmd\web\main.go
+
 func main() {
 
-	http.HandleFunc("/", HomePage)
-	http.HandleFunc("/about", AboutPage)
+	http.HandleFunc("/", handlers.HomePage)
+	http.HandleFunc("/about", handlers.AboutPage)
 	fmt.Printf("Starting server on port %s", "http://localhost"+PortNumber)
 	_ = http.ListenAndServe(PortNumber, nil)
 }
--- htmltemplatesgo/pkg/handlers/handlers.go
+++ htmltemplatesgo/pkg/handlers/handlers.go
@@ -1,14 +1,15 @@
-package main
+package handlers
 
 import (
+	"htmltemplatesgo/pkg/render"
 	"net/http"
 )
 
 func HomePage(w http.ResponseWriter, r *http.Request) {
 	// w.Write([]byte("Hello, World!"))
-	renderTemplate(w, "home.page.tmpl")
+	render.RenderTemplate(w, "home.page.tmpl")
 }
 
 func AboutPage(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "about.page.tmpl")
+	render.RenderTemplate(w, "about.page.tmpl")
 }
